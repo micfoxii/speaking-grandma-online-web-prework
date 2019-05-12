0698bd6acd75d8bd771c6ddf6074f5f4e4c06506
Michael Fox <michael.c.fox.ii@gmail.com>
2019-05-12 03:47:22 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(str)
   if str.capitalize
     "HUH?! SPEAK UP, SONNY!"
-  if str == "I LOVE YOU GRANDMA!"
+  elsif str == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
 end
 end
