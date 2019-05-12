44a43b76e57a885d781bed6f3816ceca2792d78f
Michael Fox <michael.c.fox.ii@gmail.com>
2019-05-12 04:02:56 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,6 +5,8 @@ def speak_to_grandma(str)
     "NO, NOT SINCE 1938!"
   elsif str == "WHAT?"
     "HUH?! SPEAK UP, SONNY!"
+  else
+    "HUH?! SPEAK UP, SONNY!"
 end
 end
 # Whatever you say to grandma, she should respond with
