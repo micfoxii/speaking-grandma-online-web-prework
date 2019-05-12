215494687a888597f3d78a07479357ae2d8b589d
Michael Fox <michael.c.fox.ii@gmail.com>
2019-05-12 03:32:14 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,4 @@
- def speak_to_grandma
-if speak_to_grandma =="I LOVE YOU GRANDMA!"
-  "I LOVE YOU TOO PUMPKIN!"
-elsif speak == speak.upcase
-  "NO, NOT SINCE 1938!"
-else speak == speak.capitalize
-  "HUH?! SPEAK UP, SONNY!"
-end
+
 
   
 # Whatever you say to grandma, she should respond with
